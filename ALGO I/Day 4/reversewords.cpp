fd04c7c6e8c2f5a5e42acb7889b206f8e41285d0
Vivek Kumar <75789755+virecker@users.noreply.github.com>
2022-07-19 10:18:07 +0530

    Create reversewords.cpp

---
+++ ALGO I/Day 4/reversewords.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+  string reverseWords(string s) {
+        int j=0;
+       for(int i=0;i<s.size();i++)
+       {
+           if(s[i]==' ')
+           {
+               int k=i-1;
+                while(j<k)
+                {
+                swap(s[j++],s[k--]);
+                }
+                j=i+1;
+           } 
+       }
+      int p = s.size()-1;
+      int m = j;
+      while(m<p)
+      {
+          swap(s[m++],s[p--]);
+      }
+        return s;
+    }
+};
