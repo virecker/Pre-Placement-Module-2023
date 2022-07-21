c13520abf42f53e4e902fd2fa66026d244c36047
Vivek Kumar <75789755+virecker@users.noreply.github.com>
2022-07-21 10:59:24 +0530

    Create firstun.cpp

---
+++ DS I/Day 6/firstun.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> freq; 
+        for (auto& ch : s) ++freq[ch]; 
+        for (int i = 0; i < s.size(); ++i) 
+            if (freq[s[i]] == 1) return i; 
+        return -1; 
+    }
+};
