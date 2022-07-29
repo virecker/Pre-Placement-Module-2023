fd7a0bff3fdc966045a73277418cd588074d5310
Vivek Kumar <75789755+virecker@users.noreply.github.com>
2022-07-29 17:01:44 +0530

    Create insrtbnrtr.cpp

---
+++ DS I/Day 13/insrtbnrtr.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+       
+        if(root==NULL)
+        {
+            root=new TreeNode(val);
+            return root;
+        }
+        
+        if(val>root->val){
+            root->right=insertIntoBST(root->right,val);
+        }else{
+            root->left=insertIntoBST(root->left,val);
+        }
+        return root;
+    }
+};
